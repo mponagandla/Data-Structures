8c3dd73a0cfcb36c159fa230f4c2eb57b2fbb882
mjay768 <MJL!ves4Ever>
2021-01-30 17:11:38 -0600

    Linked List : findVal() feature added (New) - Recursive

--- LinkedList.py
+++ LinkedList.py
@@ -1,11 +1,15 @@
+import sys
+
+sys.setrecursionlimit(5000)
+
 class Node:
     def __init__(self, data = None, next = None, prev = None):
         self.data = data
         self.next = next
         self.prev = prev
 
-
 class LinkedList:
+    headtrck = None
     def __init__(self):
         self.head = None
         self.tail = None
@@ -14,24 +18,26 @@ class LinkedList:
             node = Node(data, next = self.head)
             self.head = node
             self.tail = node
+            self.headtrck = node
         else:
             node = Node(data, self.head)
             self.head.prev = node
             self.head = node
+            self.headtrck = node
 
     def insertAtEnd(self, data):
         if self.head == None:
             node = Node(data,prev = self.tail)
             self.head = node
             self.tail = node
-            print("none")
+            self.headtrck = head
             return
         node = Node(data,prev = self.tail)
         self.tail.next = node
         self.tail = node
 
 
-    def printLR(self, rev = False):
+    def printLR(self,head, rev = False):
         if self.head == None:
             print("List Empty")
             return
@@ -65,14 +71,39 @@ class LinkedList:
             itr = itr.prev
         print(llstr)
 
+    def updateVal(self,nodeval, newval):
+        if self.head == None:
+            print("List is empty")
+            return
+        itr = self.head
+        while itr:
+            if itr.data == nodeval:
+                itr.data = newval
+                print("Value updated")
+                return
+            itr = itr.next
+        print("Entered node cannot be found")
+        return
+
+    def findVal(self,headtrck,val):
+        if headtrck == None:
+            print("List Empty")
+        elif headtrck.data == val:
+                print("Element exists")
+        elif headtrck == self.tail:
+            print("EOL Reached")
+            return
+        else: self.findVal(headtrck.next,val)
+
 
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insertAtStart(13)
     ll.insertAtStart(10)
     ll.insertAtStart(11)
-    ll.insertAtStart(12)
-    ll.insertAtEnd(9)
+    #ll.insertAtStart(12)
+    #ll.insertAtEnd(9)
     #ll.insertAtEnd(10)
-    ll.printRL(rev = True)
-    ll.printLR()
+    ll.printRL(rev = False) # Print from Right to Left
+    #ll.printLR()
+    ll.findVal(ll.headtrck,12)
